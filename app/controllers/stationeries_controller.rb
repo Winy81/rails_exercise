e2c62a9b2efc93fd9fa3da2b68a2971c9cf86cc7
iSmash <nemeth.richo@gmail.com>
2019-05-25 14:33:59 +0100

    validations and method refactoring for redirection

--- app/controllers/stationeries_controller.rb
+++ app/controllers/stationeries_controller.rb
@@ -19,7 +19,7 @@ class StationeriesController < ApplicationController
 	  	redirect_to stationeries_path
 	  else
 	  	flash.now[:danger] = "Item hasn't been created"
-	  	render :new
+	  	redirect_to new_stationery_path
 	  end
 	end
 
@@ -34,7 +34,7 @@ class StationeriesController < ApplicationController
 	  	redirect_to stationeries_path(@stationery)
 	  else
 	  	flash.now[:danger] = "Item hasn't been updated"
-	  	render :edit
+	  	redirect_to edit_stationery_path
 	  end
 	end
 
--- app/models/stationery.rb
+++ app/models/stationery.rb
@@ -1,2 +1,6 @@
 class Stationery < ApplicationRecord
+
+	validates_presence_of :name, message: "Name can't be blank"
+	validates_presence_of :product_code, message: "Product Code can't be blank"
+
 end
