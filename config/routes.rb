4e7290da5d4f4286f918092483e6eee754c70186
iSmash <nemeth.richo@gmail.com>
2019-05-26 16:37:13 +0100

    User controller and index - show action and views for test

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in:'login',sign_out:'logut',sign_up:'register'}
   root to: "stationeries#index"
   resources :stationeries
+  resources :users, only: [:index, :show]
 
 end
