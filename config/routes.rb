e7054df055af63a7029b67f78a3031f379750219
iSmash <nemeth.richo@gmail.com>
2019-05-26 11:19:24 +0100

    user routes changing more

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users, path: '', path_names: {sign_in:'login',sign_out:'logut'}
+  devise_for :users, path: '', path_names: {sign_in:'login',sign_out:'logut',sign_up:'register'}
   root to: "stationeries#index"
   resources :stationeries
 
