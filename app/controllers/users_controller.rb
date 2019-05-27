5f3e64c993310f2d81ee6ee98737f34d652dd668
iSmash <nemeth.richo@gmail.com>
2019-05-27 17:47:37 +0100

    setup admin's edit filed and edit-update-destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,40 @@ class UsersController < ApplicationController
 	  end
 	end
 
+	def edit
+	  if logged_in?(:admin)
+	  	@user = User.find(params[:id])
+	  else
+		redirect_to stationeries_path, notice: "You are not allowed to editing Users"
+	  end
+
+	end
+
+	def update
+	  @user = User.find(params[:id])
+	  if @user.update(user_params)
+	  	flash[:success] = "User has been updated"
+	  	redirect_to users_path
+	  else
+	  	flash.now[:danger] = "User hasn't been updated"
+	  	render :edit
+	  end
+	end
+
+	def destroy
+	  if logged_in?(:admin)
+	  	@user = User.find(params[:id])
+	  	if @user.destroy
+	  	  flash[:success] = "Item has been destroyed"
+	  	  redirect_to users_path
+	  	end
+	  end
+	end
+
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email,:name,:activity)
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   
-  has_many :stationery
+  has_many :stationery, dependent: :destroy
 
   ############################################################################################
   ## PeterGate Roles                                                                        ##
