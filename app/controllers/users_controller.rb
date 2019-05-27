7605a17c20cdf90d1ec55d438de7f1b7ff12be52
iSmash <nemeth.richo@gmail.com>
2019-05-27 11:11:16 +0100

    link setup for user deatils and filter logic correction on the user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,11 @@ class UsersController < ApplicationController
 	end
 
 	def show
-	  if logged_in?(:admin) || current_user	
-		@user = User.find(params[:id])
+	  if logged_in?(:admin) || current_user
+	  	@user = User.find(params[:id])
+		redirect_to stationeries_path, notice: "You are not allowed to check other user details" unless @user == current_user || logged_in?(:admin) 
 	  else
-		redirect_to stationeries_path, notice: "You are not authiruzed for listing Users"
+		redirect_to stationeries_path, notice: "You are not authiruzed for checking Users"
 	  end
 	end
 end
