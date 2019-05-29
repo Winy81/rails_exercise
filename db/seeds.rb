1ca84be71898ae943cd2d4d26f39fa799a531ec5
Adam Nemeth <nemeth.richo@gmail.com>
2019-05-29 16:26:39 +0100

    3 users has been seutp in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+ email: "test1@test.com",
+ password: "password",
+ password_confirmation: "password",
+ name: "Admin_User",
+ roles: "admin",
+ activity: true
+)
+
+puts "admin user has been created"
+
+User.create!(
+ email: "test2@test.com",
+ password: "password",
+ password_confirmation: "password",
+ name: "User",
+ roles: "editor",
+ activity: true
+)
+
+puts "regular user has been created"
+
+User.create!(
+ email: "test3@test.com",
+ password: "password",
+ password_confirmation: "password",
+ name: "Blocked_User",
+ roles: "editor",
+ activity: false
+)
+
+puts "forbidden user has been created"
+
+
+
+
+
