1ba2e6f44edca3fb632f7b37696e47a586209717
Adam Nemeth <nemeth.richo@gmail.com>
2019-05-29 10:07:35 +0100

    borrowed time checker method and test it with staionary#show

--- app/helpers/stationeries_helper.rb
+++ app/helpers/stationeries_helper.rb
@@ -1,2 +1,13 @@
 module StationeriesHelper
+require 'date'
+
+  def date_time_now
+    date_now = Date.parse(Time.now.to_s)
+  end
+
+  def time_different(date)
+    registered_date = Date.parse(date.to_s)
+    (date_time_now - registered_date).to_i
+  end
+
 end
